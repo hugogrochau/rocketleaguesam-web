c0edbe2b50319c8ac62861986aa64a3fd2cb4858
Hugo Grochau <hugo@grochau.com>
2017-01-28 00:32:45 -0200

    Fix missing T in BUCKET

--- internals/scripts/deploy.sh
+++ internals/scripts/deploy.sh
@@ -5,5 +5,5 @@ tar -xaf "google-cloud-sdk.tar.gz"
 ./google-cloud-sdk/install.sh --usage-reporting false --path-update true
 source './google-cloud-sdk/path.bash.inc'
 gcloud auth activate-service-account $TRAVIS_SERVICE_ACCOUNT --key-file=travis-service-account-auth.json
-gsutil -m rsync -R -d build/ $GCS_BUCKE
+gsutil -m rsync -R -d build/ $GCS_BUCKET
 
