26be8552a7fc45592412c6a49f29f9489945efe7
Hugo Grochau <hugo@grochau.com>
2017-02-21 12:24:49 -0300

    Fix Players test

--- app/containers/Players/tests/index.test.js
+++ app/containers/Players/tests/index.test.js
@@ -1,28 +1,24 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import { mountWithIntlMui } from '../../../../internals/testing/enzymeHelpers';
 
 import { Players } from '../index';
+import OrderedTable from '../../../components/OrderedTable';
 
 injectTapEventPlugin();
 
 describe('<Players />', () => {
-  it('Should call fetch players on mount', () => {
-    const mockFunc = jest.fn();
-    mountWithIntlMui(
-      <Players
-        fetchPlayers={mockFunc}
-      />
-    );
-    expect(mockFunc).toHaveBeenCalled();
-  });
-
-  it('Should render the OrderedTable', () => {
-    const renderedComponent = shallow(
-      <Players />
-    );
-    expect(renderedComponent.find('OrderedTable').length).toBe(1);
-  });
+  const mockFunc = jest.fn();
+  const renderedComponent = mountWithIntlMui(
+    <Players
+      fetchPlayers={mockFunc}
+    />
+  );
+  it('Should call fetch players on mount', () =>
+    expect(mockFunc).toHaveBeenCalled()
+  );
+  it('Should render OrderedTable', () =>
+    expect(renderedComponent.find(OrderedTable).length).toBe(1)
+  );
 });
