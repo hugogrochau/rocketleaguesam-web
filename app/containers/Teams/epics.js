8b641d9311d2eeb93e62d9845f8ded762da6a9c4
Hugo Grochau <hugo@grochau.com>
2017-02-21 22:19:05 -0300

    Prevent action from being passed as a query parameter in the teams fetch request

--- app/containers/Teams/epics.js
+++ app/containers/Teams/epics.js
@@ -9,7 +9,7 @@ import { teamsFetchSucceeded, teamsFetchFailed } from './actions';
 
 const fetchTeamsEpic = (action$, store, api) =>
   action$.ofType(TEAMS_FETCH_REQUESTED)
-    .mergeMap(api.team.all)
+    .mergeMap(() => api.team.all())
     .map((response) => response.data.teams)
     .map((teams) => teamsFetchSucceeded(teams))
     .catch((err) => Observable.of(teamsFetchFailed(err.data)));
