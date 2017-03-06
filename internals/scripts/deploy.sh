ee71fd1f5b0aaa253fd8866962f6117f42657d4c
Hugo Grochau <hugo@grochau.com>
2017-03-06 14:34:27 -0300

    Remove comments from deploy script for production

--- internals/scripts/deploy.sh
+++ internals/scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# wget "https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz"
-# tar -xaf "google-cloud-sdk.tar.gz"
-# ./google-cloud-sdk/install.sh --usage-reporting false --path-update true
-# source './google-cloud-sdk/path.bash.inc'
-# gcloud auth activate-service-account $TRAVIS_SERVICE_ACCOUNT --key-file=travis-service-account-auth.json
+wget "https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz"
+tar -xaf "google-cloud-sdk.tar.gz"
+./google-cloud-sdk/install.sh --usage-reporting false --path-update true
+source './google-cloud-sdk/path.bash.inc'
+gcloud auth activate-service-account $TRAVIS_SERVICE_ACCOUNT --key-file=travis-service-account-auth.json
 
 # backup build folder
 mkdir build.bak/
